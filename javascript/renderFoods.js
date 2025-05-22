6e371056b808acc731411eaa905d1c2620f3a52a
Mario <mariopelaeztrevejo@gmail.com>
2025-05-22 21:13:27 +0200

    Dia 4 - mas logica y funcionalidades para los objetos anteriormente añadidos

--- javascript/renderFoods.js
+++ javascript/renderFoods.js
@@ -12,6 +12,29 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("dialprix:", dialprix);
     console.log("dia:", dia);
 
+    // 
+    const box = document.createElement("div");
+    box.id = "custom-box";
+    box.style.display = "flex";
+    box.style.cssText = `
+    display:none; position:fixed; top:0; left:0; font-size:10px;
+    background:rgba(0,0,0,0.5); justify-content:center; align-items:center;
+    
+  `;
+    box.innerHTML = `
+    <div style="background:#fff; padding:20px; border-radius:8px; max-width:400px; width:80%;">
+      <h2 style="display:flex; justify-self:center;" id="box-title" style="margin-top:0;"></h2>
+      <p style="padding:8px 0 8px 0;" id="box-body"></p>
+      <span style="display:flex; justify-self:flex-end; " id="box-supermarket"></span>
+      <button style="display:flex; justify-self:center; border:none; border-radius:4px;" id="box-close">Cerrar</button>
+    </div>
+  `;
+    document.body.appendChild(box);
+
+
+    document.getElementById("box-close").onclick = () => box.style.display = "none";
+
+
     //-----------------------------------------
     const data = JSON.parse(localStorage.getItem("foods")) || [];
 
@@ -23,9 +46,20 @@ document.addEventListener("DOMContentLoaded", () => {
             item.classList.add("product");
             item.innerHTML = `
       <h3 class="food-name">${food.description.slice(0, 20)}</h3>
-      <span class="food-supermarket">${food.supermarket.toUpperCase()}<span>
+      <span class="food-supermarket">${food.supermarket.toUpperCase()}</span>
+      <div class="add-list-button">+</div>
 
     `;
+            item.style.cursor = "pointer";
+            item.onclick = () => {
+                document.getElementById("box-title").textContent = food.description.slice(0, 20);
+                document.getElementById("box-body").textContent = food.foodNutrients.map(i => i.name).join(", ");
+                document.getElementById("box-supermarket").textContent = food.supermarket.toUpperCase();
+                box.style.display = "flex";
+            };
+
+            
+
             if (food.supermarket === "carrefour") {
                 carrefour?.append(item);
                 // console.log(item);
